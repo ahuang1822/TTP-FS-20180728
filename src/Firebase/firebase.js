2b1d899779a922c911ad4dc6d8f7a8c9f4136938
Anthony Huang <ahuang1822@gmail.com>
2018-11-20 02:28:00 -0500

    firebase config hidden

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,14 +1,6 @@
 import * as firebase from 'firebase';
 import 'firebase/firestore';
-
-const config = {
-  apiKey: "AIzaSyC2fjMJFltK64HHQXYHx5C92pNj60bQLVU",
-  authDomain: "stock-portfolio-app.firebaseapp.com",
-  databaseURL: "https://stock-portfolio-app.firebaseio.com",
-  projectId: "stock-portfolio-app",
-  storageBucket: "stock-portfolio-app.appspot.com",
-  messagingSenderId: "852875937901"
-};
+import config from './config';
 
 firebase.initializeApp(config);
 
